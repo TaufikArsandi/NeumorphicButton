9b3a6d1d8e2d611b7ad33eff7618274826be2d81
Sandi <taufikarsandi@gmail.com>
2021-02-01 06:49:13 +0700

    add button view 4

---
+++ NeumorphicButton/NeumorphicButton/ButtonView4.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonView4.swift
+//  NeumorphicButton
+//
+//  Created by taufik arsandi on 01/02/21.
+//
+
+import SwiftUI
+
+struct ButtonView4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonView4_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView4()
+    }
+}
